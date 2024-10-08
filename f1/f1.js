c97ba0bd5b5e1c4771702b06ea1c2ae7a9fb5f89
kispruki <kispruki@gmail.com>
2024-10-08 08:57:57 +0200

    solution

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,21 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+
+
 async function f1 (lat, long, timeStr) {
-    //TODO
+    let addr = APIAdress + `latitude=${lat}&longitude=${long}` +
+        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+
+    const resp = await fetch(addr)
+    .then(data=>data.json());
+    
+    let arr = resp.hourly.temperature_2m;
+    let s = 0;
+    arr.forEach(element => {
+        s+=element;
+    });
+
+    return Math.floor(s/arr.length);
 }
 
 module.exports = f1;
